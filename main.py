66123bd1c6584dd0ac7de8d8688a6ae31cbad4a1
Jonatan Moncada <jimoncadar@unicah.edu>
2026-03-25 16:16:10 -0600

    Agregando las demas columnas y definiendo sus tamaños

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ from ttkbootstrap.constants import *
 
 root = ttk.Window()
 root.title = "Proyectito"
-root.geometry("400x400")
+root.geometry("900x400")
 
 ttk.Label(root, text="Empleado", font=('Sans Serif', 14)).place(x=150, y=5)
 
 ttk.Label(root, text="ID:").place(x=30, y=50)
-txtID = ttk.Entry(root, state=READONLY).place(x=110, y=40, width=150)
+txtID = ttk.Entry(root, state=READONLY).place(x=110, y=40, width=200)
 
 ttk.Label(root, text="Código").place(x=30, y=90)
-txtCodigo = ttk.Entry(root).place(x=110, y=80, width=150)
+txtCodigo = ttk.Entry(root).place(x=110, y=80, width=200)
 
 ttk.Label(root, text="Empleado:").place(x=30, y=130)
-txtEmpleado = ttk.Entry(root).place(x=110, y=120, width=150)
+txtEmpleado = ttk.Entry(root).place(x=110, y=120, width=200)
 
 ttk.Label(root, text="Puesto:").place(x=30, y=170)
-txtPuesto = ttk.Entry(root).place(x=110, y=160, width=150)
+txtPuesto = ttk.Entry(root).place(x=110, y=160, width=200)
 
 ttk.Label(root, text="Departamento:").place(x=30, y=210)
-txtDepartamento = ttk.Entry(root).place(x=140, y=200, width=120)
+txtDepartamento = ttk.Entry(root).place(x=140, y=200, width=170)
 
 ttk.Label(root, text="Salario:").place(x=30, y=250)
-txtSalario = ttk.Entry(root).place(x=110, y=240, width=150)
+txtSalario = ttk.Entry(root).place(x=110, y=240, width=200)
 
 tree = ttk.Treeview(
     root,
@@ -45,10 +45,11 @@ tree.heading("departamento", text="Departamento")
 tree.heading("salario", text="Salario")
 
 tree.column("id", width=30, stretch=False)
-tree.column("codigo", width=40)
-""" tree.column("tamaño", width=110, anchor="e")
-tree.column("modificado", width=140, anchor="center")
-tree.column("tipo", width=110, anchor="w") """
+tree.column("codigo", width=20)
+tree.column("empleado", width=20)
+tree.column("puesto", width=20)
+tree.column("departamento", width=20)
+tree.column("salario", width=20)
 
 
 
